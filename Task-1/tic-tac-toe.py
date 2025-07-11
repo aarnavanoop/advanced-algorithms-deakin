740a67b7bf8be5bb94152caf5fed73e535821685
aarnavanoop <aarnavanoop@gmail.com>
2025-07-11 23:13:11 +1000

    define function to get empty cells

--- Task-1/tic-tac-toe.py
+++ Task-1/tic-tac-toe.py
@@ -9,3 +9,28 @@ def print_board(board):
     for row in board:
         print(" | ".join(row))
         print("-" * 9)
+
+
+def check_winner(board, player):
+    """Checks if the specified player has won."""
+    # Check rows, columns, and diagonals
+    win_conditions = [
+        [board[0][0], board[0][1], board[0][2]],
+        [board[1][0], board[1][1], board[1][2]],
+        [board[2][0], board[2][1], board[2][2]],
+        [board[0][0], board[1][0], board[2][0]],
+        [board[0][1], board[1][1], board[2][1]],
+        [board[0][2], board[1][2], board[2][2]],
+        [board[0][0], board[1][1], board[2][2]],
+        [board[0][2], board[1][1], board[2][0]],
+    ]
+    return [player, player, player] in win_conditions
+
+def get_empty_cells(board):
+    """Returns a list of all empty cells."""
+    empty_cells = []
+    for r in range(3):
+        for c in range(3):
+            if board[r][c] == " ":
+                empty_cells.append((r, c))
+    return empty_cells
